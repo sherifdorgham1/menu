41b35eb5fd6690fd58ddb49241e8b99200c52e16
sherifdorgham1 <sheriftop01@gmail.com>
2026-04-18 22:38:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const sheetURL = "https://opensheet.elk.sh/1k6NxRHJYTSZtmAqkB-P6Qrpylnh5crkaIGkqGjtCoTA/1";
 
 let data = [];
@@ -14,9 +13,15 @@ function show(cat){
   let html = "";
 
   data.filter(x => x["القسم"] == cat).forEach(item => {
+
+    // fallback لو الصورة فاضية أو بايظة
+    let img = item["الصورة"] ? 
+    `https://images.weserv.nl/?url=${item["الصورة"]}` :
+    "https://via.placeholder.com/300";
+
     html += `
     <div class="card">
-      <img src="https://images.weserv.nl/?url=${item["الصورة"]}">
+      <img src="${img}">
       <div class="card-content">
         <div class="name">${item["اسم"]}</div>
         <div class="price">${item["السعر"]} جنيه</div>
