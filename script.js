b7e6478a1586778216c3fc06126c9d30e9ff7640
sherifdorgham1 <sheriftop01@gmail.com>
2026-04-18 22:03:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+const sheetURL = "https://opensheet.elk.sh/1k6NxRHJYTSZtmAqkB-P6Qrpylnh5crkaIGkqGjtCoTA/1";
+
+let data = [];
+
+fetch(sheetURL)
+.then(res => res.json())
+.then(res => {
+  data = res;
+  show("طيور");
+});
+
+function show(cat){
+  let html = "";
+
+  data.filter(x => x["القسم"] == cat).forEach(item => {
+    html += `
+    <div class="card">
+      <img src="https://images.weserv.nl/?url=${item["الصورة"]}">
+      <div class="card-content">
+        <div class="name">${item["اسم"]}</div>
+        <div class="price">${item["السعر"]} جنيه</div>
+      </div>
+    </div>
+    `;
+  });
+
+  document.getElementById("content").innerHTML = html;
+}
