cada27d27575838bc51a05306230327d6f045c39
sherifdorgham1 <sheriftop01@gmail.com>
2026-04-19 02:55:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,19 +31,24 @@ function show(cat){
     `https://images.weserv.nl/?url=${item["الصورة"]}` :
     "https://via.placeholder.com/300";
 
-html += `
-<div class="card">
-  <img src="${img}">
-  <div class="card-content">
-    <div class="name">${item["اسم"]}</div>
-    <div class="price">${item["السعر"]} جنيه</div>
+    html += `
+    <div class="card">
+      <img src="${img}">
+      <div class="card-content">
+        <div class="name">${item["اسم"]}</div>
+        <div class="price">${item["السعر"]} جنيه</div>
 
-    <a href="https://wa.me/201211340121?text=${encodeURIComponent(
-      "السلام عليكم، عايز أطلب " + item["اسم"] + " بسعر " + item["السعر"] + " جنيه"
-    )}" target="_blank" class="order-item-btn">
-      اطلب ده
-    </a>
+        <a href="https://wa.me/201211340121?text=${encodeURIComponent(
+          "السلام عليكم، عايز أطلب " + item["اسم"] + " بسعر " + item["السعر"] + " جنيه"
+        )}" target="_blank" class="order-item-btn">
+          اطلب ده
+        </a>
 
-  </div>
-</div>
-`;
+      </div>
+    </div>
+    `;
+    
+  });
+
+  document.getElementById("content").innerHTML = html;
+}
